1e902db760c221adcbe3c85d109e5ee62b8ac7d3
jaseem-jazz <jaseemjazz9746@gmail.com>
2026-09-04 15:09:34 +0530

    Time: 181 ms (87.46%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,9 +1,3 @@
-WITH first as
-    (SELECT * FROM Cinema WHERE id%2 != 0 )
-SELECT 
-    * 
-FROM 
-    first 
-WHERE 
-    description != 'boring' 
-    ORDER BY rating DESC;
+-- Write your PostgreSQL query statement below
+WITH first as (SELECT * FROM Cinema WHERE id%2 != 0 )
+SELECT * FROM first WHERE description != 'boring' ORDER BY rating DESC;
