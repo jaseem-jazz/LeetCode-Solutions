37d94d9c944b608718c133ff5cfe9b75797b432b
jaseem-jazz <jaseemjazz9746@gmail.com>
2026-09-04 15:05:51 +0530

    Time: 308 ms (56.68%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,3 @@
--- Write your PostgreSQL query statement below
--- with man as(select managerId, count(managerId) as count from Employee  group by managerID having count(managerId) > 4)
--- select e.name from employee e left join man m on e.managerId = m.managerid where e.id = m.managerid
 SELECT
     e.name 
 FROM 
